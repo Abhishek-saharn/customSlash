783630702564220019bffbfd3f3785e468c9a530
abhishek.saharn <abhishek.saharn@gmail.com>
2017-10-12 13:19:03 +0530

    changes done

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ app.post('/', (req, res) => {
         "devesh": "offsite",
         "vinay": "day off",
     }
+    let attachments = {
+        "working from home": "https://ichef-1.bbci.co.uk/news/660/media/images/75292000/jpg/_75292103_80410876.jpg",
+        "onsite": "https://tr2.cbsistatic.com/hub/i/r/2015/02/03/ae3b8050-af60-440b-a397-ac6061d06166/resize/770x/d88a5e21c251cf863d59417739ef7f37/officeworkersistock000044385306small.jpg",
+        "offsite": "https://img.etimg.com/thumb/msid-35402653,width-300,imglength-37812,resizemode-4/indian-technology-firms-sending-fewer-employees-overseas-due-to-cost-pressure.jpg",
+        "day off": "http://lipstickchica.com/wp-content/uploads/2016/09/day-off.jpg",
+
+    }
     // console.log("inside jdkfsjkdhkdshfkhdskfh")
     let text = req.body.text;
 
@@ -29,11 +36,13 @@ app.post('/', (req, res) => {
         res.send('U R DOIN IT WRONG. Enter a text not a number.');
         return;
     }
+    status = workstatus[text];
     let data = {
         response_type: 'in_channel', // public to the channel 
-        text: `${text} is ${workstatus[text]}`,
+        text: `${text} is ${status}`,
         attachments: [{
-            image_url: 'https://ichef-1.bbci.co.uk/news/660/media/images/75292000/jpg/_75292103_80410876.jpg'
+
+            image_url: `${attachments[status]}`
         }]
     };
 
