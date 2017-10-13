6d76f469e97df0631243fb63e5f42774809b2d99
abhishek.saharn <abhishek.saharn@gmail.com>
2017-10-13 16:45:30 +0530

    Added buttons

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -118,7 +118,7 @@ export const approvedAction = (req, res) => {
     request.post('https://slack.com/api/chat.update', data, (error, response, body) => {
 
     });
-    console.log(">>>>><<<<<<<<", data, "_______________", req.body);
+    console.log(">>>>><<<<<<<<", data, "_______________", req.body.payload);
     // res.json(req.body);
 
 }
